81a2ff0cc0a6daa79fe2bfd7354f91d9ddd851dd
Fazil M <dumpdusty@gmail.com>
2024-05-16 00:27:21 -0700

    refactor DatePicker Page

---
+++ pom/utils/constants.ts
@@ -0,0 +1,18 @@
+export const Links  = {
+  header: 'h1',
+  fromInput: "#from",
+  toInput: "#to",
+  monthOfTheYear: '[class="ui-datepicker-month"]',
+  prevYear: '[title="Prev"]',
+  nextYear: '[title="Next"]',
+  getDate: ".ui-state-default",
+  dateFromComponent: "#ui-datepicker-div",
+  dateYear: '[class="ui-datepicker-year"]',
+  dateMonth: '[class="ui-datepicker-month"]',
+  selectedMonth: '[selected="selected"]',
+
+}
+
+export const monthList = {
+  Jan: '01', Feb: '02', Mar: '03', Apr: '04', May: '05', Jun: '06', Jul: '07', Aug: '08', Sep: '09', Oct: '10', Nov: '11', Dec: '12'
+}
--- tests/pageLayouts.spec.ts
+++ tests/pageLayouts.spec.ts
@@ -2,15 +2,13 @@ import {test} from '@playwright/test'
 // @ts-ignore
 import colors = require('colors')
 
-test.beforeEach(async({page}) => {
-  await page.goto('localhost:4200/pages/forms/layouts')
-})
 
-test.describe.only('PAGE LAYOUTS', () => {
-  test('inline form', async ({page}) => {
+test.describe('PAGE LAYOUTS', () => {
+  test.only('inline form', async ({page}) => {
+    await page.goto('localhost:4200/pages/forms/layouts')
     await page.locator('[placeholder="Jane Doe"]').fill('Jack Sparrow')
     await page.locator('[type="text"][placeholder="Email"]').fill('jacksparrow@pirate.com')
-    // await page.getByRole('checkbox').nth(0).check()
+    await page.locator('.custom-checkbox').nth(0).check()
   })
 
   test('using the grid', async ({page}) => {
